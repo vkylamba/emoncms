92b9cabdfec240cc7b2c17588bcd0822cc9fc697
Glyn Hudson <glyn.hudson@gmail.com>
2015-06-09 23:49:09 +0100

    add note regarding using emonPi Emoncms updater with RFM69Pi

--- Modules/admin/admin_main_view.php
+++ Modules/admin/admin_main_view.php
@@ -26,7 +26,8 @@
     <tr>
         <td>
             <h3><?php echo _('Update emonPi'); ?></h3>
-            <p><?php echo _('Downloads latest changes from Github and runs emonPi update script'); ?></p>
+            <p>Downloads latest Emoncms changes from Github and updates emonPi firmware. See imprtant notes in <a href="https://github.com/openenergymonitor/emonpi/blob/master/Atmega328/emonPi_RFM69CW_RF12Demo_DiscreteSampling/compiled/CHANGE%20LOG.md">emonPi firmware change log.</a></p>
+	    <p>Note: If using emonBase (Raspberry Pi + RFM69Pi) the updater can still be used to update Emoncms, RFM69Pi firmware will not be changed.</p> 
             <p>View the update logfile (/var/log/emonpiupdate.log) <a href="<?php echo $path; ?>admin/getemonpiupdatelog">here</a></p>
             <div class="alert alert-info" id="emonpiupdatereply" style="display:none"></div>
         </td>
